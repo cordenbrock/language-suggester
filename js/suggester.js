cbb8aa18ef58a29d11b2e815ca61fab06495fb6f
cordenbrock <cordenbrock@gmail.com>
2020-10-23 11:36:34 -0700

    establish functions to conditionally handle and store user inputs

--- js/suggester.js
+++ js/suggester.js
@@ -1,5 +1,6 @@
 $(document).ready(() => {
 // form submission
+
     $("#form").submit((e) => {
         e.preventDefault();
         console.log("successful submission");
@@ -8,10 +9,10 @@ $(document).ready(() => {
         answerOne = $("#q1").val();
         answerTwo = $("input:radio[name=radio]:checked").val();
         answerThree = $("#q3").val();
-        answerFour = $("#q4").val();
+        answerFour = parseInt($("#q4").val());
         console.log(answerOne, answerTwo, answerThree, answerFour);
     // analyze user answers
-        function  ansOne() {
+        function ansOne() {
             if (answerOne === "1") {
                 return "java";
             } else if (answerOne === "2") {
@@ -22,10 +23,54 @@ $(document).ready(() => {
                 $("#q1").after("all fields must be completed");
             }
         };
-        resultOne = ansOne();
-        console.log(resultOne);
+
+        function ansTwo() {
+            if (answerTwo === "a") {
+                return "java";
+            } else if (answerTwo === "b") {
+                return "python";
+            } else if (answerTwo === "c") {
+                return "cobol";
+            } else {
+                $("#q2").after("all fields must be completed");
+            }
+        };
+
+        console.log(answerThree.length)
+        function ansThree() {
+            if (answerThree.length <= 5) {
+                return "java";
+            } else if (answerThree.length>5 && answerThree.length<=10) {
+                return "python";
+            } else if (answerThree.length > 10) {
+                return "cobol";
+            };
+        };
+
+        function ansFour() {
+            if (answerFour < 0) {
+                return "java";
+            } else if (answerFour>0 && answerFour<100) {
+                return "python";
+            } else if (answerFour >= 100) {
+                return "cobol";
+            } else {
+                $("#q4").after("all fields must be completed");
+            }
+        }
+
+        const resultOne = ansOne();
+        const resultTwo = ansTwo();
+        const resultThree = ansThree();
+        const resultFour = ansFour();
+
+        // if (resultOne === resultTwo) {
+        //     return resultOne
+        // } else
+
 
         // print results
-        $("#print-results").text("test-print")
+        $("#print-results").text(`${resultOne} ${resultTwo} ${resultThree} ${resultFour}`)
     });
+
 });
